c0caf8da3de80a10261410cd55456665bc1a6a2a
Adhidarma Hadiwinoto <gua@adhisimon.org>
2018-04-12 19:45:17 +0700

    advice

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ process.chdir(__dirname);
 const pullgw = require('komodo-sdk/gateway/pull');
 const partner = require('./lib/partner');
 const reverseReport = require('./lib/reverse-report');
+const adviceServer = require('komodo-sdk/gateway/advice-push-server');
 
 pullgw.setPartner(partner);
+adviceServer.setPartner(partner);
