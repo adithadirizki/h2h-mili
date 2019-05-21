06b2ccea2371af64efcc64629db8f991ecb7d316
Adhidarma Hadiwinoto <me@adhisimon.org>
2019-05-21 15:50:06 +0700

    eslint

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ process.chdir(__dirname);
 const fs = require('fs');
 fs.writeFileSync('pid.txt', process.pid);
 
-const apiserver = require('komodo-sdk/api-server');
+require('komodo-sdk/api-server');
 const pullgw = require('komodo-sdk/gateway/pull');
 const partner = require('./lib/partner');
-const reverseReport = require('./lib/reverse-report');
+require('./lib/reverse-report');
 const adviceServer = require('komodo-sdk/gateway/advice-push-server');
 
 pullgw.setPartner(partner);
