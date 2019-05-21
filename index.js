afd80f70b85a745cfadc8957973af1911fa50970
Adhidarma Hadiwinoto <me@adhisimon.org>
2019-05-21 16:02:52 +0700

    support irs reverse report

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ process.chdir(__dirname);
 const fs = require('fs');
 fs.writeFileSync('pid.txt', process.pid);
 
+const config = require('komodo-sdk/config');
+const logger = require('komodo-sdk/logger');
+
 require('komodo-sdk/api-server');
 const pullgw = require('komodo-sdk/gateway/pull');
 const partner = require('./lib/partner');
-require('./lib/reverse-report');
 const adviceServer = require('komodo-sdk/gateway/advice-push-server');
+
+if (config.partner && config.partner.reverse_report_irs) {
+    logger.info('Reverse report using IRS mode');
+    require('./lib/irs/reverse-report');
+}
+else {
+    require('./lib/reverse-report');
+}
+
 
 pullgw.setPartner(partner);
 adviceServer.setPartner(partner);
