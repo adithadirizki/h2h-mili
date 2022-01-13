a676a55e80c2deae4b58e6c2eb0b38cbc4780c8c
Dawang Komodo <dawang@komodo>
2022-01-13 10:43:16 +0700

    Cleanup

--- index.js
+++ index.js
@@ -8,21 +8,12 @@ const config = require('komodo-sdk/config');
 
 global.KOMODO_LOG_LABEL = `KOMODO-GW@${ config.handler_name }`;
 
-const logger = require('komodo-sdk/logger');
-
 require('komodo-sdk/api-server');
 const pullgw = require('komodo-sdk/gateway/pull');
-const partner = require('./lib/partner');
 const adviceServer = require('komodo-sdk/gateway/advice-push-server');
 
-if (config.partner && config.partner.reverse_report_irs) {
-    logger.info('Reverse report using IRS mode');
-    require('./lib/irs/reverse-report');
-}
-else {
-    require('./lib/reverse-report');
-}
-
+const partner = require('./lib/partner');
+require('./lib/reverse-report');
 
 pullgw.setPartner(partner);
 adviceServer.setPartner(partner);
